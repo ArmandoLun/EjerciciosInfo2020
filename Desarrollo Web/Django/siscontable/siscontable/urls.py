de735f8b6342f4a74173954e82fd25a100e46614
Luna Armando Gabriel <armandogabrielluna@gmail.com>
2020-08-28 18:46:48 -0300

    html_views

--- Desarrollo Web/Django/siscontable/siscontable/urls.py
+++ Desarrollo Web/Django/siscontable/siscontable/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cuentas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hoy/', views.hoy),
+    path('cuentas/', views.cuentas),
 ]
